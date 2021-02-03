d7af2701c8cb804aa03751b092c41eeb97a8b689
Isamu Ito <isamsito@gmail.com>
2021-02-03 23:22:02 +0000

    eqObjects logic works for basic data types in objects

--- eqObjects.js
+++ eqObjects.js
@@ -13,7 +13,15 @@ const eqObjects = function(object1, object2) {
   const objectLength1 = Object.keys(object1).length;
   const objectLength2 = Object.keys(object2).length;
   if(objectLength1 === objectLength2){
-    console.log('lengths match');
+    // iterate through keys in one object
+    for(const key in object1){
+      if(object1[key] !== object2[key]){
+        return false;
+      } 
+    }
+    return true
+  } else {
+    return false;
   }
 };
 

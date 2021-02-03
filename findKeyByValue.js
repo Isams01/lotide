30c1b870455ad30b8ba77914bc2148c4c042a3c9
Isamu Ito <isamsito@gmail.com>
2021-02-03 22:50:04 +0000

    remove debug statements

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -13,16 +13,3 @@ const findKeyByValue = function(object, value) {
     }
   }
 }
-
-const sampleObject = {
-  a: 1,
-  b: 3,
-  c: 3,
-  show: "brooklyn 99",
-  testing: "lighthouse in the house",
-}
-
-assertEqual(findKeyByValue(sampleObject, 'brooklyn 99'), 'show');
-assertEqual(findKeyByValue(sampleObject, 3), 'b');
-assertEqual(findKeyByValue(sampleObject, 1), 'a');
-assertEqual(findKeyByValue(sampleObject, 'lighthouse in the house'), 'testing');

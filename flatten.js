0ffd3bccce84359cc5807a83ce43b0dc5f74b04c
Isamu Ito <isamsito@gmail.com>
2021-02-03 15:51:28 +0000

    Add middle.js and flatten array working

--- flatten.js
+++ flatten.js
@@ -2,15 +2,19 @@
 const flatten = (arrays) => {
   let flattenedArray = [];
   arrays.forEach(element => {
-    element.forEach(subEl => {
-      flattenedArray.push(subEl);
-    });
+    if(Array.isArray(element)){
+      element.forEach(subEl => {
+        flattenedArray.push(subEl);
+      });
+    } else {
+      flattenedArray.push(element);
+    }
   });
   return flattenedArray
 };
 
 
-const sample = [[1,2], [1,2,3], [3], [4,3,2,1]];
+const sample = [9,8,7,[1,2], [1,2,3], 3, [4,3,2,1],['a','b','c']];
 // should get back [1,2,1,2,3,3,4,3,2,1];
 
 console.log(flatten(sample));
---
+++ middle.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const asserArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+const middle = function(array){
+  if(array.length <= 2){
+    return [];
+  }
+  const midIndex = Math.floor(array.length/2);
+  let isEven = false;
+  array.length%2 === 0 ? isEven = true : isEven = false;
+  if(!isEven){
+    return [array[midIndex]]
+  } else {
+    return [array[midIndex - 1], array[midIndex]]
+  }
+}
+
+
+middle([0]);            // 0
+middle([0,1]);          // 1
+console.log(middle([0,1,2]));        // 1
+console.log(middle([0,1,2,3]));      // 2
+console.log(middle([0,1,2,3,4]));    // 2
+
+asserArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+asserArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+asserArraysEqual(middle([1, 2]), []);

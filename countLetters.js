82521b4815c83ac4fc497e3313ad0ba75721d92b
Isamu Ito <isamsito@gmail.com>
2021-02-03 21:32:05 +0000

    Use regex to remove spaces in string

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,19 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const countLetters = function(letters) {
+  let result = {};
+  let noSpace = letters.replace(/\s/g, '');
+  for(let i = 0; i < letters.length; i++){
+    if(result[letters[i]]){
+      result[letters[i]] += 1;
+    } else {
+      result[letters[i]] = 1;
+    }
+  }
+  return result;
+};
+
+
+console.log(countLetters('hhhb bbc'));

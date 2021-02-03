2d987baca351912a241138d51c2898dbdaaf410b
Isamu Ito <isamsito@gmail.com>
2021-02-03 01:05:22 +0000

    without js working

--- without.js
+++ without.js
@@ -18,16 +18,24 @@ const asserArraysEqual = function(arr1, arr2) {
   }
 };
 
+
 const without = function(arr, removeVars) {
   let newArr = []
   for(let i = 0; i < arr.length; i++){
-    for(let j = 0; j < removeVars.length; j++){
-      if(removeVars[j] !== arr[i]){
-        newArr.push(arr[i]);
-      }
+    if(!removeVars.includes(arr[i])){
+      newArr.push(arr[i]);
     }
   }
+  return newArr;
 }
+
+const originalArray = [1,2,3,4,5,6,7,8,9,'a','b','3',"hello", 'HeLLo'];
+console.log(`original Array: ${originalArray}`)
+console.log(without(originalArray, [3,4,'3', 'hello']));
+
+
+
+
 
 // This function should take in a source array and a itemsToRemove array. 
 // It should return a new array with only those elements from source that 

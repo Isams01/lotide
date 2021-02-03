24909d8f78f8654f536e352aeb9072ff6be4fce2
Isamu Ito <isamsito@gmail.com>
2021-02-03 23:15:49 +0000

    check if lengths match

--- eqObjects.js
+++ eqObjects.js
@@ -10,4 +10,20 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
+  const objectLength1 = Object.keys(object1).length;
+  const objectLength2 = Object.keys(object2).length;
+  if(objectLength1 === objectLength2){
+    console.log('lengths match');
+  }
 };
+
+const ab = {a: 1, b: 2}
+const ba = {b:2, a:1}
+const abString = {a: "1", b: "2"}
+
+const abc = {a:1,b:2,c:3}
+
+assertEqual(eqObjects(ab, ba),true); // true
+assertEqual(eqObjects(ab, abc),false); // false
+assertEqual(eqObjects(ab, abString),false); // false
+

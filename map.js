d2803f697c71dc65b2ad44f42004e065c51ecd70
Isamu Ito <isamsito@gmail.com>
2021-02-04 20:31:09 +0000

    following along with tutorial

---
+++ map.js
@@ -0,0 +1,40 @@
+// Our map function will take in two arguments:
+
+//     An array to map
+//     A callback function
+
+const eqArrays = function(arr1, arr2) {
+  if(arr1.length != arr2.length){
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if(eqArrays(arr1,arr2)){
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+const map = (array, cb) => {
+  let results = [];
+  for (let item of array) {
+    results.push(cb(item));
+  }
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+

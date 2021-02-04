edaec61eec00e19de1558edb37aab4c8a4f0281d
Isamu Ito <isamsito@gmail.com>
2021-02-04 21:14:30 +0000

    tests commented out

--- takeUntil.js
+++ takeUntil.js
@@ -31,17 +31,17 @@ const takeUntil = function(array, callback) {
   return result;
 }
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-assertArraysEqual(results1, [1,2,5,7,2]);
-console.log('---');
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
+// assertArraysEqual(results1, [1,2,5,7,2]);
+// console.log('---');
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+// assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
 
-const testArr = ['a','bb','ccc','dddd','eeeee']
-assertArraysEqual(takeUntil(testArr, letters => letters.length > 3), ['a','bb','ccc']);
-assertArraysEqual(takeUntil(testArr, letters => letters.length > 10), ['a','bb','ccc','dddd','eeeee']);
+// const testArr = ['a','bb','ccc','dddd','eeeee']
+// assertArraysEqual(takeUntil(testArr, letters => letters.length > 3), ['a','bb','ccc']);
+// assertArraysEqual(takeUntil(testArr, letters => letters.length > 10), ['a','bb','ccc','dddd','eeeee']);

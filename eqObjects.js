eed6242b9cdb9adfea2f809cd9093caca5fddf13
Isamu Ito <isamsito@gmail.com>
2021-02-04 02:00:06 +0000

    eqObjects.js linted

--- eqObjects.js
+++ eqObjects.js
@@ -7,11 +7,11 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  if(arr1.length != arr2.length){
+  if (arr1.length !== arr2.length) {
     return false;
   }
-  for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -24,39 +24,39 @@ const eqArrays = function(arr1, arr2) {
 const eqObjects = function(object1, object2) {
   const objectLength1 = Object.keys(object1).length;
   const objectLength2 = Object.keys(object2).length;
-  if(objectLength1 === objectLength2){
+  if (objectLength1 === objectLength2) {
     // iterate through keys in one object
-    for(const key in object1){
+    for (const key in object1) {
       // first check if its an array
-      if(Array.isArray(object1[key])){
-        if(!eqArrays(object1[key],object2[key])){
-          return false
+      if (Array.isArray(object1[key])) {
+        if (!eqArrays(object1[key],object2[key])) {
+          return false;
         }
       } else {
-        if(object1[key] !== object2[key]){
+        if (object1[key] !== object2[key]) {
           return false;
-        } 
-      } 
+        }
+      }
     }
     // if it makes it through the object without returning false then they are equal
-    return true
+    return true;
   } else {
     return false;
   }
 };
 
-const ab = {a: 1, b: 2}
-const ba = {b:2, a:1}
-const abString = {a: "1", b: "2"}
-const abc = {a:1,b:2,c:3}
+const ab = {a: 1, b: 2};
+const ba = {b:2, a:1};
+const abString = {a: "1", b: "2"};
+const abc = {a:1,b:2,c:3};
 
 const arrAB = {a:["1", 2], b: 2, c:'ab'};
 const arrBA = {b: 2, a:["1", 2], c:'ab'};
-const arrCString = {a: ['1','2'], b: ['2','1']}
-const arrC = {a: [1,2], b: [2,1]}
-const test1 = {a:[1,2], b:2}
-const test2 = {a: 1, b: 2}
-const test3 = {b: 2, a:[1,2]}
+const arrCString = {a: ['1','2'], b: ['2','1']};
+const arrC = {a: [1,2], b: [2,1]};
+const test1 = {a:[1,2], b:2};
+const test2 = {a: 1, b: 2};
+const test3 = {b: 2, a:[1,2]};
 
 console.log('Testing basic data types');
 assertEqual(eqObjects(ab, ba),true); // true

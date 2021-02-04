25ad9d04fafb41f529157f7319e42e7921b0ed44
Isamu Ito <isamsito@gmail.com>
2021-02-04 21:42:57 +0000

    tests passed

--- findKey.js
+++ findKey.js
@@ -8,16 +8,34 @@ const assertEqual = function(actual, expected) {
 
 const findKey = (obj, cb) => {
   for (key in obj) {
-    console.log(key);
+    if(cb(obj[key])){
+      return key;
+    }
   }
 };
 
 
-findKey({
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)
+}, x => x.stars === 2), 'noma');
+
+const testObj = {
+  hello: 'world',
+  a: 2,
+  b: 3
+}
+
+assertEqual(findKey(testObj, x => x===2), 'a');
+
+const lodashTest = {
+  'barney':  { 'age': 36, 'active': true },
+  'fred':    { 'age': 40, 'active': false },
+  'pebbles': { 'age': 1,  'active': true }
+}
+
+assertEqual(findKey(lodashTest, x => x.active===true), 'barney');

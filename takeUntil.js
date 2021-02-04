8a9e626662784408c4d167828ca7eb88ac8f23e7
Isamu Ito <isamsito@gmail.com>
2021-02-04 21:05:10 +0000

    initiating tests

--- takeUntil.js
+++ takeUntil.js
@@ -20,17 +20,20 @@ const assertArraysEqual = function(arr1, arr2) {
 
 
 const takeUntil = function(array, callback) {
-  // ...
-
+  
 
 }
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 console.log(results1);
-
+assertArraysEqual(results1, [1,2,5,7,2]);
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+const testArr = ['a','bb','ccc','dddd','eeeee']
+assertArraysEqual(takeUntil(testArr, letters => letters.length > 3), ['a','bb','ccc']);

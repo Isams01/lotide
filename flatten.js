fa77b295a78ad725688e1ae3724ae3517c59a160
Isamu Ito <isamsito@gmail.com>
2021-02-08 23:55:03 +0000

    flatten uses recursion and somehow works

--- flatten.js
+++ flatten.js
@@ -1,21 +1,15 @@
-// iterate through array of arrays and return single array
 const flatten = (arrays) => {
   let flattenedArray = [];
   arrays.forEach(element => {
     if(Array.isArray(element)){
-      element.forEach(subEl => {
-        flattenedArray.push(subEl);
-      });
+      flattenedArray.push(...flatten(element)); // it works and I dont know why
     } else {
       flattenedArray.push(element);
+      // console.log(element);
     }
   });
   return flattenedArray
 };
 
-
-const sample = [9,8,7,[1,2], [1,2,3], 3, [4,3,2,1],['a','b','c']];
-// should get back [1,2,1,2,3,3,4,3,2,1];
-
-console.log(flatten(sample));
+module.exports = flatten;
 

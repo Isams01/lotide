87570a3707f6fd8b5ca51de48a614b307084cf46
Isamu Ito <isamsito@gmail.com>
2021-02-08 20:21:28 +0000

    I think I added mocha chai this commit

--- test/eqArrayTest.js
+++ test/eqArrayTest.js
@@ -1,2 +1,16 @@
 const assertEqual = require('../assertEqual');
 const eqArray = require('../eqArray');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('eqArray', () =>{
+  it('should return true when two arrays are equal', () =>{
+    assert.isTrue(eqArray([1,2,3],[1,2,3]));
+  });
+  it('should return false when two arrays are not the same', () => {
+    assert.isFalse(eqArray([3,2,1], [1,2,3]));
+  });
+  it('should return false when the arrays are different lengths', () => {
+    assert.isFalse(eqArray([3,2,1,0], [3,2,1]));
+  });
+})

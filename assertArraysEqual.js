d5e9b394de1939a77718f5fe3b3b440d4b991750
Isamu Ito <isamsito@gmail.com>
2021-02-08 20:27:38 +0000

    assertArrayEqual modularized

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,11 @@
-const eqArrays = function(arr1, arr2) {
-  if(arr1.length != arr2.length){
-    return false;
-  }
-  for(let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true;
-};
+const eqArray = require('./eqArray');
 
 const assertArraysEqual = function(arr1, arr2) {
-  if(eqArrays(arr1,arr2)){
+  if(eqArray(arr1,arr2)){
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
+
+module.exports = assertArraysEqual;

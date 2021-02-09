f843af3bf47c7a5d353984b09052f39cfa3ff178
Isamu Ito <isamsito@gmail.com>
2021-02-09 04:25:21 +0000

    refactor and set tests

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArray');
   
 
 // Returns true if both objects have identical keys with identical values.
@@ -43,30 +25,5 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-const ab = {a: 1, b: 2};
-const ba = {b:2, a:1};
-const abString = {a: "1", b: "2"};
-const abc = {a:1,b:2,c:3};
-
-const arrAB = {a:["1", 2], b: 2, c:'ab'};
-const arrBA = {b: 2, a:["1", 2], c:'ab'};
-const arrCString = {a: ['1','2'], b: ['2','1']};
-const arrC = {a: [1,2], b: [2,1]};
-const test1 = {a:[1,2], b:2};
-const test2 = {a: 1, b: 2};
-const test3 = {b: 2, a:[1,2]};
-
-console.log('Testing basic data types');
-assertEqual(eqObjects(ab, ba),true); // true
-assertEqual(eqObjects(ab, abc),false); // false
-assertEqual(eqObjects(ab, abString),false); // false
-console.log('');
-console.log('testing with arrays as values');
-assertEqual(eqObjects(arrAB, arrBA),true); // true
-assertEqual(eqObjects(arrAB, arrC),false); // false
-assertEqual(eqObjects(arrBA, arrC),false); // false
-assertEqual(eqObjects(arrCString, arrC),false); // false
-assertEqual(eqObjects(test1, test2),false); // false
-assertEqual(eqObjects(test2, test1),false); // false
-assertEqual(eqObjects(test3, test1),true); // false
+module.exports = eqObjects;
 
--- test/eqObjects.test.js
+++ test/eqObjects.test.js
@@ -1,26 +1,36 @@
-const ab = {a: 1, b: 2};
-const ba = {b:2, a:1};
-const abString = {a: "1", b: "2"};
-const abc = {a:1,b:2,c:3};
+const eqObjects = require('../eqObjects');
+const assert = require('chai').assert;
 
-const arrAB = {a:["1", 2], b: 2, c:'ab'};
-const arrBA = {b: 2, a:["1", 2], c:'ab'};
-const arrCString = {a: ['1','2'], b: ['2','1']};
-const arrC = {a: [1,2], b: [2,1]};
-const test1 = {a:[1,2], b:2};
-const test2 = {a: 1, b: 2};
-const test3 = {b: 2, a:[1,2]};
+describe('eqObjects', () => {
+  it('should return true for opposite objects', () => {
+    const ab = {a: 1, b: 2};
+    const ba = {b:2, a:1};
+    assert.isTrue(eqObjects(ab,ba));
+  });
+  it('should return false for objects of different sizes', () => {
+    const ab = {a: 1, b: 2};
+    const abc = {a:1,b:2,c:3};
+    assert.isFalse(eqObjects(ab,abc));
+  });
+  it('should return false for objects with different datatypes', () => {
+    const ab = {a: 1, b: 2};
+    const abString = {a: "1", b: "2"};
+    assert.isFalse(eqObjects(ab,abString));
+  });
+  it('should return True for values as arrays', () => {
+    const arrAB = {a:["1", 2], b: 2, c:'ab'};
+    const arrBA = {b: 2, a:["1", 2], c:'ab'};
+    assert.isTrue(eqObjects(arrAB,arrBA));
+  });
+  it('should return False for values with arrays but different sizes', () => {
+    const arrAB = {a:["1", 2], b: 2, c:'ab'};
+    const arrC = {a: [1,2], b: [2,1]};
+    assert.isFalse(eqObjects(arrAB,arrC));
+  });
+  it('should return False for values with arrays but different datatypes', () => {
+    const arrCString = {a: ['1','2'], b: ['2','1']};
+    const arrC = {a: [1,2], b: [2,1]};
+    assert.isFalse(eqObjects(arrCString, arrC));
+  });
 
-console.log('Testing basic data types');
-assertEqual(eqObjects(ab, ba),true); // true
-assertEqual(eqObjects(ab, abc),false); // false
-assertEqual(eqObjects(ab, abString),false); // false
-console.log('');
-console.log('testing with arrays as values');
-assertEqual(eqObjects(arrAB, arrBA),true); // true
-assertEqual(eqObjects(arrAB, arrC),false); // false
-assertEqual(eqObjects(arrBA, arrC),false); // false
-assertEqual(eqObjects(arrCString, arrC),false); // false
-assertEqual(eqObjects(test1, test2),false); // false
-assertEqual(eqObjects(test2, test1),false); // false
-assertEqual(eqObjects(test3, test1),true); // false
+})
